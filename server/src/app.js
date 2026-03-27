84d98811631d464f887d797cf5ec8e5ffbf40de6
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-03-27 10:29:22 +0530

    Updated CORS for frontend deployment

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://ims-frontend.onrender.com"
+    "https://ims-frontend-3pzv.onrender.com"
   ],
   credentials: true
 }))
