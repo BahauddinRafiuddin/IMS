26fad28a65430dd249633e006e06d32c67f779af
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-03-27 11:35:37 +0530

    Fix Brevo config with chaning port

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -5,7 +5,7 @@ export const sendEmail = async (to, subject, html) => {
 
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
       user: process.env.BREVO_USER,
