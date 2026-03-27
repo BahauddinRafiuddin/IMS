26277b375cef80e12b2dbd23147177bc4520f311
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-03-27 11:28:18 +0530

    Fix Brevo config

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, html) => {
   console.log("Sending email to:", to);
+
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
@@ -9,13 +10,17 @@ export const sendEmail = async (to, subject, html) => {
     auth: {
       user: process.env.BREVO_USER,
       pass: process.env.BREVO_PASS
-    }
+    },
+    connectionTimeout: 20000,
+    greetingTimeout: 20000
   });
 
   await transporter.sendMail({
-    from: `"IMS Platform" <${process.env.EMAIL_USER}>`,
+    from: `"IMS Platform" <${process.env.BREVO_USER}>`,
     to,
     subject,
     html
   });
+
+  console.log("Email sent successfully");
 };
